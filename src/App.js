44a80ec11548d22b1217de955bae022ebc96f006
ajitesh <ajitesh314mishra@gmail.com>
2022-04-10 00:54:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [quote, setQuote] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loadingQuote, setLoadingQuote] = useState(false);
+
+  useEffect(() => {
+    fetchRandomQuote();
+  }, []);
+
+
+
+  async function fetchRandomQuote() {
+    try {
+      setLoadingQuote(true);
+      setErrorMessage("");
+      const quoteObject = await axios.get("https://api.quotable.io/random");
+      setQuote(quoteObject.data);
+      setLoadingQuote(false);
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoadingQuote(false);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='advice_parent'>
+        <div className='advice_card'>
+          <h1>" {quote.content} "</h1>
+
+          <button className='advice_button' onClick={fetchRandomQuote}>GET ADVIDE</button>
+        </div>
+      </div>
     </div>
   );
 }
